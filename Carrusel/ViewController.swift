3e4a47022619778ce3adc1d63798ca40df4d847b
Kike01 <kf7@outlook.es>
2016-11-18 11:39:53 -0500

    g ,g , ,g , , ,g g,

--- Carrusel/ViewController.swift
+++ Carrusel/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController, iCarouselDelegate,iCarouselDataSource {
         return image.count
     }
     
-    // aumento de tamaño de image
+    // aumento de tamaño de imagekmvkmvkmvtlv,lmlbml tkmbmblmbk
     func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
         let tempView = UIView(frame: CGRect(x: 0, y: 0, width: 130, height: 130))
         
